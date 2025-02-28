4a5a54019e4afd427965f7ab3501cccb87a3425f
PabloLahoz <lahoz.pablo.smr@gmail.com>
2025-02-28 01:09:57 +0100

    blades varios

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.layout>
+    <h1>Contacto</h1>
+    <p>En Palets Épila, estamos aquí para ayudarte. Si necesitas más información sobre nuestros productos y servicios,
+        o quieres solicitar un pedido, no dudes en contactarnos.</p>
+    <h2>📍 Dirección</h2>
+    <p>Avenida Opel España Épila</p>
+    <h2>📞 Teléfono</h2>
+    <p>619247657</p>
+    <h2>📧 Correo Electrónico</h2>
+    <p>paletsepila@gmail.com</p>
+</x-layouts.layout>
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,1 +1,8 @@
-<?php
+<x-layouts.layout>
+    <h1>Noticias</h1>
+    <h2>Últimas noticias</h2>
+    <p>En Palets Épila, siempre estamos innovando y mejorando nuestros servicios para ofrecer la mejor calidad a nuestros clientes.
+        Aquí encontrarás las últimas novedades sobre nuestra empresa, nuevos productos y tendencias en el sector de los palets.</p>
+    <h2>Nuevas soluciones en palets reciclados</h2>
+    <p>Estamos ampliando nuestra oferta de palets reciclados, apostando por la sostenibilidad y el cuidado del medioambiente.</p>
+</x-layouts.layout>
